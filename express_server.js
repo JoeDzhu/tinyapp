e7426c4ffd61669341a8adbedfb62d5378fe7203
Joe Dzhu <jiayi2.zhu@ryerson.ca>
2019-07-08 22:42:45 +0000

    redirect down

--- express_server.js
+++ express_server.js
@@ -1,15 +1,20 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(2,8);
+}
 
 app.get("/", (req, res) => {
   res.send("Hail Hydra!");
@@ -27,10 +32,21 @@ app.get("/urls",(req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
 });
-  
-app.get("/urls/:shortURL", (req, res) =>{
-  let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  res.redirect("urls");
+})
+  
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  console.log("show");
   res.render('urls_show', templateVars);
 });
   
