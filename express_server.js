1ec8bf97c97f88d92d4b9c5477f12fc6f6c53ad0
Joe Dzhu <jiayi2.zhu@ryerson.ca>
2019-07-09 00:00:32 +0000

    updated

--- express_server.js
+++ express_server.js
@@ -25,7 +25,7 @@ app.get('/urls.json', (req, res) =>{
 })
 
 app.get('/hello', (req, res) =>{
-  res.send("<html><body>Hail <b>Hydra</b></body></html>")
+  res.send("<html><body><b>Hail Hydra!</b></body></html>")
 })
 
 app.get("/urls",(req, res) => {
@@ -38,16 +38,21 @@ app.get("/urls/new", (req, res) => {
 })
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  
-  urlDatabase[generateRandomString()] = req.body.longURL;
-  res.redirect("urls");
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+
+  res.redirect(`/urls/${shortURL}`);
 })
   
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  console.log("show");
   res.render('urls_show', templateVars);
+  
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
   
 app.listen(PORT, () => {
